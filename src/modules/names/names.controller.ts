d4720cd4a41623cb04683856ccb9252722171215
TaniaGA01 <tanygaitan@gmail.com>
2023-10-27 17:20:09 +0200

    crud

--- src/modules/names/names.controller.ts
+++ src/modules/names/names.controller.ts
@@ -1,4 +1,4 @@
-import { Body, Controller, Post } from '@nestjs/common';
+import { Body, Controller, Delete, Get, Param, Post, Put, Query } from '@nestjs/common';
 import { NamesService } from './names.service';
 
 @Controller('api/v1/names')
@@ -13,4 +13,29 @@ export class NamesController {
     createName(@Body() data:{name:string}){
         return this.namesService.createName(data.name);
     }
+
+    @Get()
+    //@Query('start') => 'start' is a parameter for filter data. In Postman {{server}}:{{port}}/api/v1/names?start=Ju
+    getNames(@Query('start') start:string){
+        return this.namesService.getNames(start);
+    }
+
+    @Put('/:name/:newName')
+    updateName(@Param('name') name:string, @Param('newName') newName:string){
+        return this.namesService.updateName(name,newName)
+    }
+
+    @Delete('clear') //this one must to be placed before all delete functions
+    clearNames(){
+        return this.namesService.clearNames()
+    }
+
+    @Delete('/:name')
+    deleteName(@Param('name') name:string){
+        return this.namesService.deleteName(name)
+    }
+
+    
+
 }
+
--- src/modules/names/names.service.ts
+++ src/modules/names/names.service.ts
@@ -2,18 +2,64 @@ import { Injectable } from '@nestjs/common';
 
 @Injectable()
 export class NamesService {
+  private _names: string[];
 
-    private _names: string[];
+  constructor() {
+    this._names = [];
+  }
 
-    constructor(){
-        this._names = []
+  //Create an EndPoint in controller and in service
+  createName(name: string) {
+
+    const nameFound = this._names.find(el => el.toLocaleLowerCase().trim() === name.toLocaleLowerCase().trim());
+
+    if(!nameFound){
+      this._names.push(name);
+  
+      return true;
+
+    }else{
+      return false;
     }
 
-    //Create an EndPoint in controller and in service
-    createName(name:string){
-        this._names.push(name);
-        console.log('Names:', this._names);
-        return true;
-    }
+  }
 
+  getNames(start?: string){
+
+    if(!start){
+      return this._names
+    }else{
+      return this._names.filter(el => 
+        el.toLocaleLowerCase().trim().startsWith(start.toLocaleLowerCase().trim())
+      )
+    }
+  }
+
+  updateName(name:string,newName:string){
+      const indexNameFound = this._names.findIndex(el => el.toLocaleLowerCase().trim() === name.toLocaleLowerCase().trim());
+      const indexNewNameFound = this._names.findIndex(el => el.toLocaleLowerCase().trim() === newName.toLocaleLowerCase().trim());
+      console.log(indexNameFound)
+      if(indexNameFound !== -1 && indexNewNameFound === -1){
+        
+        this._names[indexNameFound] = newName
+        return true
+      }else{
+        return false
+      }
+  }
+
+  deleteName(name:string){
+
+    const deletedBefore = this._names.length
+    this._names = this._names.filter(el => el.toLocaleLowerCase().trim() !== name.toLocaleLowerCase().trim())
+    const deletedAfter = this._names.length
+
+    return deletedBefore !== deletedAfter
+
+  }
+
+  clearNames(){
+    this._names = []
+    return true
+  }
 }
